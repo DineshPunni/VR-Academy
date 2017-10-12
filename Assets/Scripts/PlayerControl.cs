78c66817d2d0d9077a6859bb936d2a49a388fbe0
Dinesh <dinesh.punni@hotmail.de>
2017-10-12 14:21:16 +0200

    Playercontroller added

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	public Transform shootOffset;
+	public float power;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			ThrowBall();
+		}
+	}
+
+	void ThrowBall()
+	{
+		GameObject tmpBall = Instantiate(ballPrefab, shootOffset.position, shootOffset.transform.rotation);
+		tmpBall.GetComponent<Rigidbody>().velocity = shootOffset.forward * power;
+	}
+}
