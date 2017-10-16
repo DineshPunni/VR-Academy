06cab5103fb0225bef22a7984b26700f79fab1da
Dinesh <dinesh.punni@hotmail.de>
2017-10-16 14:08:24 +0200

    Score handler added: added canvas and text and assigned score handler on it

---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour {
+
+	Text scoreText;
+	int score = 0;
+
+
+	void OnEnable()
+	{
+		EnemyControl.OnEnemyDied += IncreaseScore;
+		GameManager.OnRestart += ResetScore;
+	}
+
+	void OnDisable()
+	{
+		EnemyControl.OnEnemyDied -= IncreaseScore;
+		GameManager.OnRestart -= ResetScore;
+	}
+
+	void Start ()
+	{
+		scoreText = GetComponent<Text>();
+	}
+	
+
+	void IncreaseScore()
+	{
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+
+	void ResetScore()
+	{
+		score = 0;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+}
